781e435b8fc49c0b0db84bd2d1656859f290f36c
lucky <lucky@lucky-book.local>
2025-11-08 00:34:17 +0400

    update set_env script

--- caddy/set_env.sh
+++ caddy/set_env.sh
@@ -32,8 +32,7 @@ gen_xhttp_path() {
 gen_ws_path() {
     local context="$1"
     local prefix="${WS_PREFIXES[$RANDOM % ${#WS_PREFIXES[@]}]}"
-    local token
-    token="$(hmac_hex "$HMAC_SECRET_KEY" "$context" 8)"
+    local token="$(hmac_hex "$HMAC_SECRET_KEY" "$context" 8)"
     if (( RANDOM % 2 )); then
         echo "/$prefix/$token"
     else
@@ -50,13 +49,14 @@ set_env() {
 main() {
     if [ -z "$HMAC_SECRET_KEY" ]; then
         HMAC_SECRET_KEY="$(openssl rand -hex 16)"
+        echo "[*] empty hmac secret, using: $HMAC_SECRET_KEY"
     fi
     set_env "PROXY_XHTTP_PATH" "$(gen_xhttp_path "xhttp")"
     set_env "PROXY_XHTTP_WARP_PATH" "$(gen_xhttp_path "xhttp-warp")"
     set_env "PROXY_WS_PATH" "$(gen_ws_path "ws")"
     set_env "PROXY_WS_WARP_PATH" "$(gen_ws_path "ws-warp")"
     set_env "CDN_AUTH_TOKEN" "$(hmac_hex "$HMAC_SECRET_KEY" "cdn-auth-token" 32)"
-    echo "done"
+    echo "[+] done"
 }
 
 main "$@"
