cf00c468028d104a470d4bd09c5a6f2b3d21b42c
lucky <lucky@lucky-book.local>
2025-11-13 16:50:12 +0400

    add dnscontrol

---
+++ dns/dnscontrol/dnsconfig.js
@@ -0,0 +1,24 @@
+var REG_NONE = NewRegistrar("none");
+var DNS_CLOUDFLARE = NewDnsProvider("cloudflare");
+
+var ip4 = IP("1.2.3.4");
+var ip6 = "0:0:0:0:0:0:0:0";
+var domain = "example.org";
+var telegram_subdomain = "tg";
+var signal_subdomain = "sig";
+
+D(domain, REG_NONE, DnsProvider(DNS_CLOUDFLARE),
+    DefaultTTL(1),
+    A("ip", ip4),
+    A("origin", ip4),
+    A(signal_subdomain, ip4),
+    A(telegram_subdomain, ip4),
+    A("@", ip4, CF_PROXY_ON),
+    AAAA("ip", ip6),
+    AAAA("origin", ip6),
+    AAAA(signal_subdomain, ip6),
+    AAAA(telegram_subdomain, ip6),
+    AAAA("@", ip6, CF_PROXY_ON),
+    CNAME("www", domain + ".", CF_PROXY_ON),
+    CNAME("cloudflare", domain + ".", CF_PROXY_ON),
+);
